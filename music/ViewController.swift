e419f9175ea14a3ad94b78fee96a0768d9052673
Alex Cho <38528052+alexcho617@users.noreply.github.com>
2023-07-24 12:24:54 +0900

    Test TextView

--- music/ViewController.swift
+++ music/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var firstImage: UIImageView!
+    @IBOutlet var firstLabel: UILabel!
+    @IBOutlet var firstButton: UIButton!
+    
+    //editable끄고 data detector 키면 시스템과 자동 연동
+    @IBOutlet var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func superViewClicked(_ sender: UITapGestureRecognizer) {
+        firstImage.isHidden.toggle()
+    }
+    
+    @IBAction func firstButtonClicked(_ sender: UIButton) {
+        
+    }
+    
 }
 
